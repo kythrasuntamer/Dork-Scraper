65ab7682fbcfda81dbb12e8a3f73bf047a92486a
Paul Michael Tidwell <121031213+kythrasuntamer@users.noreply.github.com>
2023-09-05 11:13:29 +0000

    Update dorks.py

--- dorks.py
+++ dorks.py
@@ -1,49 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
+import time
 
-# Set the base URL for the Google search
-base_url = "https://www.google.com/search?q="
+def search_google(query, num_pages):
+    base_url = "https://www.google.com/search?q="
+    results = []
 
-# Define a list of dorks you want to search for
-dorks = [
-    "Index of /password",
-    "Index of /admin",
-    "Index of /backup",
-    # Add more dorks here as needed
-]
-
-# Set the number of pages of results that you want to scrape
-num_pages = 20
-
-# Initialize a list to store the results
-results = []
-
-# Iterate over each dork
-for dork in dorks:
-    # Iterate over the pages of results
     for page in range(num_pages):
-        # Calculate the start parameter for the Google search URL
         start = page * 10
-        # Encode the query string
-        encoded_query = urllib.parse.quote(dork)
-        # Construct the full URL for the search
+        encoded_query = urllib.parse.quote(query)
         search_url = f"{base_url}{encoded_query}&start={start}"
-        # Send an HTTP request to the Google search URL
-        response = requests.get(search_url)
-        # Parse the HTML of the page
-        soup = BeautifulSoup(response.text, "html.parser")
-        # Find all of the search result links on the page
-        links = soup.find_all("a")
-        # Iterate over the links and store the result URLs
-        for link in links:
-            href = link.get("href")
-            if href and href.startswith("/url?q="):
-                # Extract and clean the URL
-                url = urllib.parse.urlparse(href[7:]).query.split('&')[0]
-                results.append(url)
 
-# Write the results to a file
-with open("results.txt", "w") as f:
-    for result in results:
-        f.write(result + "\n")
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+        try:
+            response = requests.get(search_url, headers=headers)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, "html.parser")
+            
+            links = soup.find_all("a")
+            for link in links:
+                href = link.get("href")
+                if href and href.startswith("/url?q="):
+                    url = urllib.parse.urlparse(href[7:]).query.split('&')[0]
+                    results.append(url)
+        except requests.exceptions.RequestException as e:
+            print("Error:", e)
+        
+        time.sleep(1)  # Add a 1-second delay between requests
+
+    return results
+
+def save_results(results, filename):
+    with open(filename, "w") as f:
+        for result in results:
+            f.write(result + "\n")
+
+if __name__ == "__main__":
+    dorks = [
+        "Index of /password",
+        "Index of /admin",
+        "Index of /backup",
+        # Add more dorks here as needed
+    ]
+    num_pages = 20
+
+    all_results = []
+    for dork in dorks:
+        dork_results = search_google(dork, num_pages)
+        all_results.extend(dork_results)
+
+    save_results(all_results, "results.txt")
